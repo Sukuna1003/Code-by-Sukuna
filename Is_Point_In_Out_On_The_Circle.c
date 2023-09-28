84d1af9ff8909af7870cafaffa6f892084082e7c
Sukuna1003 <145987937+Sukuna1003@users.noreply.github.com>
2023-09-28 22:09:13 +0530

    Create Is_Point_In_Out_On_The_Circle.c
    This program is supposed to find that whether the point we have given to it lie on the circle, outside the circle, or inside the circle.

---
+++ Is_Point_In_Out_On_The_Circle.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int cPower(int x, int y){
+    int result = 1;
+    if(y==0) return 1;
+    for(int i=0;i<y;i++){  
+        result*=x;
+    }
+    return result;
+}
+int main(){
+    int r,a,b,x,y;
+    printf("Enter the Center of the circle: \n");
+    scanf("%d %d",&a,&b);
+    printf("Enter the radius of the circle: \n");
+    scanf("%d",r);
+    printf("Enter the coordinates of your point: \n");
+    scanf("%d%d",&x,&y);
+    int length  = cPower(x-a,2)+cPower(y-b,2);
+    if(length==cPower(r,2))printf("Point is on the circumfrence of the circle.");
+    else if(length>r*r)printf("Point is outside of the circle.");
+    else if(length<r*r)printf("Point is inside of the circle.");
+    printf("%d",length);
+    return 0;
+}
