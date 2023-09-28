ed894b25a2f1779944cee4f9de0a79c97b12f603
Sukuna1003 <145987937+Sukuna1003@users.noreply.github.com>
2023-09-28 22:36:37 +0530

    Update ShowsTheTypeOfCharacter.c

--- ShowsTheTypeOfCharacter.c
+++ ShowsTheTypeOfCharacter.c
@@ -8,4 +8,5 @@ int main(){
     else if(x>=97 && x<=122)printf("The character is a small letter.");
     else if(x>=48 && x<=57)printf("The character is a digit.");
     else if(x>=0 && x<=47 || x>=58 && x<=64 || x>=91 && x<=96 || x>=123 && x<=127)printf("The character is a Special Symbol.");
+    return 0;
 }
