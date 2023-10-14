ed3ccfd4d1b973593d7f66f4775cf3612b892c8e
Sukuna1003 <145987937+Sukuna1003@users.noreply.github.com>
2023-10-14 10:45:48 +0530

    Update SortedArrayMerging.c

--- SortedArrayMerging.c
+++ SortedArrayMerging.c
@@ -1,36 +1,38 @@
-program online
-#include <stdio.h>
-void merge(int a[],int lena,int lenb){
-    int count =0, sort[8];
-    for(int i=0;i<lena;i++){
-        for(int j=0;J<lenb;j++){
-            if(a[i]<b[j]){
-                sort[count]=a[i];
-                break;
-            }
-            if(a[i]>b[j]){
-                sort[count]=b[j];
-                break;
-            }
-            else {
-                sort[count]=a[i];
-                sort[++count]=b[j];
-                break;
-            }
-            count++;
-        }
-    
-    }
-    for(int i=0;i<(lena+lenb);i++)
-       printf("%d",sort[i]);
+#include<stdio.h>
+void merg(int a[] ,int alength, int b[],int blength);
+int main(){
+    int arr1[] = {2,4,6,8};
+    int arr2[] = {1,3,5,7};
+    int larr1 = (sizeof(arr1)/sizeof(arr1[0]));
+    int larr2 = (sizeof(arr2)/sizeof(arr2[0]));
+    // int a = larr1+larr2;
+    merg(arr1,larr1,arr2,larr2);
+  
 }
-int main() {
-   int a[]={2,4,6,8};
-   int b[]={1,3,5,7};
-   int c= (sizeof(a)/sizeof(a[0]));
-   int d= (sizeof(b)/sizeof(b[0]));
-   merge(a,b,c,d);
-    
 
-    return 0;
+void merg(int a[],int alength, int b[],int blength){
+    int sorted[40];
+    int count=0;
+    int i=0,j=0;
+    while( i<alength && j<blength){
+        if(a[i]<b[j]){
+            sorted[count++] = a[i++];
+        }
+        else{
+            sorted[count++] = b[j++];
+        }
+    }
+    if(i==alength){
+        for(j;j<blength;j++){
+            sorted[count++] = b[j];
+        }
+    }
+    if(j==blength){
+        for(i;i<alength;i++){
+            sorted[count++] = a[i];
+        }
+    }
+    for(int i=0;i<count;i++)
+        printf("%d ",sorted[i]);
+    return ;
 }
